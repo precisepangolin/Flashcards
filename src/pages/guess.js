cf30a9b9dfe5269f073fabb7078d219c1e1c0e04
Karol Workowski <charls7c@gmail.com>
2023-08-23 15:12:59 +0200

    API DZIALA

--- src/pages/guess.js
+++ src/pages/guess.js
@@ -5,7 +5,32 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Button } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
+import axios, {get} from 'axios';
 
+window.addEventListener('load', updateWords);
+async function getWords() {
+    console.log("Running getting words f")
+    try {
+        const response = await axios.get('http://localhost:7055/words');
+        const words = response.data[0]['lastname'];
+        console.log(response.data[0]['lastname'])
+        return words;
+        // Use the data in your React components
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function updateWords() {
+    console.log("Running updating word")
+    try {
+    const word = await getWords();
+    const listElement = document.getElementById('words-list');
+    listElement.innerHTML = word;
+    } catch (err) {
+        console.error(err);
+    }
+}
 function Guess() {
     const locationState = useLocation().state;
     var counter = 1;
@@ -27,6 +52,7 @@ function Guess() {
  </Col>
  </Row>
 <Card>{locationState.item.flashcards[0].frontSide}</Card>
+          <Card id='words-list'></Card>
 <Button variant="primary">Umiem</Button> <Button variant="primary">To trudne</Button>
 
                             </Card.Body>
